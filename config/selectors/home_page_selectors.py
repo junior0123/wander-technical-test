bacbfba543dfedd01d68fe5a4bc3b85b0b619ced
Alvaro Sivila <juniorsivila123@gmail.com>
2024-10-22 08:11:48 -0400

    Create an enum with city selectors and buttons for the homepage.

---
+++ config/selectors/home_page_selectors.py
@@ -0,0 +1,41 @@
+from enum import Enum
+
+
+class HomePageButtonSelectors(Enum):
+    BUTTON_WHEREVER = ("button", "Wherever")
+    BUTTON_WHENEVER = ("button", "Whenever")
+    BUTTON_WHOEVER = ("button", "Whoever")
+    BUTTON_SEARCH = ("css", ".group\\/button")
+
+
+class HomePageCitySelectors(Enum):
+    LABEL_ALASKA = "alaska"
+    LABEL_ARIZONA = "arizona"
+    LABEL_CALIFORNIA = "california"
+    LABEL_VIRGINIA = "virginia"
+    LABEL_VERMONT = "vermont"
+    LABEL_UTAH = "utah"
+    LABEL_TEXAS = "texas"
+    LABEL_TENNESSEE = "tennessee"
+    LABEL_ST_THOMAS = "st. thomas"
+    LABEL_SOUTH_CAROLINA = "south carolina"
+    LABEL_QUINTANA_ROO = "quintana roo"
+    LABEL_OREGON = "oregon"
+    LABEL_NORTH_CAROLINA = "north carolina"
+    LABEL_NEW_YORK = "new york"
+    LABEL_NEW_JERSEY = "new jersey"
+    LABEL_NEVADA = "nevada"
+    LABEL_NAYARIT = "nayarit"
+    LABEL_MONTANA = "montana"
+    LABEL_MICHIGAN = "michigan"
+    LABEL_MASSACHUSETTS = "massachusetts"
+    LABEL_MAINE = "maine"
+    LABEL_IDAHO = "idaho"
+    LABEL_HAWAII = "hawaii"
+    LABEL_GEORGIA = "georgia"
+    LABEL_FLORIDA = "florida"
+    LABEL_CONNECTICUT = "connecticut"
+    LABEL_COLORADO = "colorado"
+    LABEL_CHIHUAHUA = "chihuahua"
+
+
